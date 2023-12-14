d1a5366ef77ae06542603ab2b079d8f952f8b448
Moses Palmér <mosespalmer@gmail.com>
2023-12-14 16:06:55 +0100

    Add tree twig

--- twigs/local/__init__.py
+++ twigs/local/__init__.py
@@ -3,9 +3,10 @@
 
 import importlib
 
+from argparse import _SubParsersAction
 from pathlib import Path
 
-from .. import empty
+from .. import Twig, empty, run
 
 
 main = empty()
@@ -16,3 +17,33 @@ for p in (
         for p in Path(__file__).parent.iterdir()
         if p.is_file() and p.suffix == '.py' and p.name[0] != '_'):
     importlib.import_module('.' + p.name.rsplit('.', 1)[0], __package__)
+
+
+def twig_main(me: Twig, **kwargs):
+    def list(**kwargs):
+        run(
+            me.name,
+            'tree', '-a',
+            '-I', '__pycache__',
+            '-I', '.gitignore',
+            me.source.parent)
+
+    {
+        'list': list,
+    }[kwargs.pop('local_command')](**kwargs)
+
+
+@main.arguments
+def arguments(me: Twig, actions: _SubParsersAction):
+    from .. import tree
+
+    if tree.main.enabled:
+        actions = actions.add_parser(me.name, help='Manages local files.') \
+            .add_subparsers(required=True, dest='local_command')
+
+        list = actions.add_parser(
+            'list',
+            help='Lists local files')
+
+        return {
+            me.name: lambda **args: twig_main(me, **args)}
