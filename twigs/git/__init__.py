978e061c3bb34af3dc59dd8fdd0a1e2acc94ab90
Moses Palmér <mosespalmer@gmail.com>
2023-12-14 16:06:55 +0100

    Add git twig

---
+++ twigs/git/__init__.py
@@ -0,0 +1,29 @@
+"""The stupid content tracker.
+"""
+
+import nest
+
+from pathlib import Path
+from typing import Any
+
+from .. import Twig, system
+
+
+#: The git directory.
+GIT_DIR = nest.ROOT / '.git'
+
+#: The work tree.
+WORK_TREE = nest.ROOT
+
+
+main = system.package()
+
+
+def command(me: Twig, *args, **kwargs) -> Any:
+    """Runs git in this repository.
+    """
+    return me.run(
+        'git',
+        '--git-dir={}'.format(GIT_DIR),
+        '--work-tree={}'.format(WORK_TREE),
+        *args, **kwargs)
