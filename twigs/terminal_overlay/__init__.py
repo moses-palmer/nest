84e6c6b4f8bc33f05560c90ddb5ad55c841b88ef
Moses Palmér <mosespalmer@gmail.com>
2023-12-14 16:06:55 +0100

    Add Terminal Overlay and associated programs twigs

---
+++ twigs/terminal_overlay/__init__.py
@@ -0,0 +1,27 @@
+"""A terminal overlay controlled by D-BUS.
+"""
+
+import re
+
+from .. import Twig, dconf, dbus_send
+
+
+main = dconf.keybindings(
+    dconf.KeyBinding(
+        name='\'Toggle Terminal Overlay\'',
+        binding='\'<Primary>section\'',
+        command='\'dbus-send '
+            '--session '
+            '--dest=com.newrainsoftware.TerminalOverlay '
+            '--type=method_call '
+            '/com/newrainsoftware/TerminalOverlay '
+            'com.newrainsoftware.TerminalOverlay.Toggle\''),
+    dconf.KeyBinding(
+        name='\'Cycle Terminal Overlay Monitor\'',
+        binding='\'<Primary>Tab\'',
+        command='\'dbus-send '
+            '--session '
+            '--dest=com.newrainsoftware.TerminalOverlay '
+            '--type=method_call '
+            '/com/newrainsoftware/TerminalOverlay '
+            'com.newrainsoftware.TerminalOverlay.CycleDisplay\''))
