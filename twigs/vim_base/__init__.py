7edd9a1f144422b9842690a52678df476797b33d
Moses Palmér <mosespalmer@gmail.com>
2025-03-17 19:16:43 +0100

    Replace vim with nvim

--- twigs/vim_base/__init__.py
+++ twigs/vim_base/__init__.py
@@ -1,7 +1,7 @@
 """A base configuration for vim.
 """
 
-from .. import Twig, git, vim, shell_utilities
+from .. import Twig, git, nvim, shell_utilities
 
 
 main = git.with_submodules(Twig.empty())
