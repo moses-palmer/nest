254ffbd2392abeaea849166c0f44a4f4fa7d0772
Moses Palmér <mosespalmer@gmail.com>
2025-03-20 17:11:02 +0100

    Replace telescope.nvim with snacks.nvim

--- twigs/nvim_neotree/__init__.py
+++ twigs/nvim_neotree/__init__.py
@@ -1,7 +1,7 @@
 """Neovim plugin to manage the file system and other tree like structures.
 """
 
-from .. import nvim, nvim_nui, nvim_web_devicons
+from .. import nvim, nvim_nui, nvim_plenary_nvim, nvim_web_devicons
 
 
 main = nvim.plugin() \
---
+++ twigs/nvim_snacks_nvim/__init__.py
@@ -0,0 +1,7 @@
+"""A collection of QoL plugins for Neovim
+"""
+
+from .. import nvim
+
+
+main = nvim.plugin()
