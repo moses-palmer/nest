aa46b3ce26447fadfc219802d9cc2622812d0c5e
Moses Palmér <mosespalmer@gmail.com>
2025-06-10 14:44:20 +0200

    Use standard line width for Rust in nvim

--- twigs/nvim_fileformats/files/.config/nvim/lua/nvim-fileformats-load/init.lua
+++ twigs/nvim_fileformats/files/.config/nvim/lua/nvim-fileformats-load/init.lua
@@ -2,3 +2,10 @@ vim.cmd('packadd! ron.vim')
 vim.cmd('packadd! rust.vim')
 
 vim.g.rustfmt_autosave = 1
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'rust',
+    callback = function()
+        vim.opt.colorcolumn = '+1'
+        vim.opt.textwidth = 99
+    end,
+})
