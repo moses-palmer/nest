8abab78ca573d21ddd599660899d88c5b6915396
Moses Palmér <mosespalmer@gmail.com>
2025-06-10 14:24:08 +0200

    Use new interface to lspconfig

--- twigs/jedi_language_server/files/.config/nvim/lua/jedi-language-server-load/init.lua
+++ twigs/jedi_language_server/files/.config/nvim/lua/jedi-language-server-load/init.lua
@@ -1,3 +1,4 @@
-require'lspconfig'.jedi_language_server.setup {
+vim.lsp.config('jedi_language_server', {
     capabilities = require'cmp_nvim_lsp'.default_capabilities()
-}
+})
+vim.lsp.enable('jedi_language_server')
