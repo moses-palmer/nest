3e23e7610e08e4b958ea018b4e5dfb3cad6e3a13
Moses Palmér <mosespalmer@gmail.com>
2025-10-23 16:00:22 +0200

    Add TypeScript Language Server twig

---
+++ twigs/typescript/__init__.py
@@ -0,0 +1,8 @@
+"""TypeScript is a superset of JavaScript that compiles to clean JavaScript output.
+"""
+
+from .. import npm
+
+
+main = npm.package(
+    name='typescript')
---
+++ twigs/typescript_language_server/files/.config/nvim/lua/typescript_language_server/init.lua
@@ -0,0 +1,4 @@
+vim.lsp.config('ts_ls', {
+    capabilities = require'cmp_nvim_lsp'.default_capabilities()
+})
+vim.lsp.enable('ts_ls')
