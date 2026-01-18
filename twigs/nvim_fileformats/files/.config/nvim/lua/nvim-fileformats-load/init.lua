82496b56fd15844c7feb1af1ed2ad5504f4ff6be
Moses Palmér <mosespalmer@gmail.com>
2026-01-18 17:45:08 +0100

    Use Rust edition 2024 for rustfmt neovim plugin

--- twigs/nvim_fileformats/files/.config/nvim/lua/nvim-fileformats-load/init.lua
+++ twigs/nvim_fileformats/files/.config/nvim/lua/nvim-fileformats-load/init.lua
@@ -1,6 +1,7 @@
 vim.cmd('packadd! ron.vim')
 vim.cmd('packadd! rust.vim')
 
+vim.g.rustfmt_options = '--edition=2024'
 vim.g.rustfmt_autosave = 1
 vim.api.nvim_create_autocmd('FileType', {
     pattern = 'rust',
