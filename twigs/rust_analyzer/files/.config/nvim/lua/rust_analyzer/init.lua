ad2fa51e5c3b21c563af4f2d4fe2b4c34fc750fa
Moses Palmér <mosespalmer@gmail.com>
2026-04-09 17:09:24 +0200

    Update rust-analyzer configuration

--- twigs/rust_analyzer/files/.config/nvim/lua/rust_analyzer/init.lua
+++ twigs/rust_analyzer/files/.config/nvim/lua/rust_analyzer/init.lua
@@ -1,4 +1,21 @@
 vim.lsp.config('rust_analyzer', {
     capabilities = require'nvim-lspconfig-load'.capabilities(),
+    settings = {
+        ['rust-analyzer'] = {
+            diagnostics = {
+                styleLints = {
+                    enable = true,
+                },
+            },
+            gotoImplementations = {
+                filterAdjacentDerives = true,
+            },
+            imports = {
+                merge = {
+                    glob = false,
+                },
+            },
+        },
+    },
 })
 vim.lsp.enable('rust_analyzer')
