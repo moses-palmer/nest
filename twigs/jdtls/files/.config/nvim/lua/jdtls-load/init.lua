7fb92e05530b546b4e3a69cc06eb262ca5ab59e2
Moses Palmér <mosespalmer@gmail.com>
2026-07-21 10:25:36 +0200

    Update twigs
    jdtls
     • 1.60.0@202606262232
    
    nvim-codecompanion-nvim
     • chore: fix CI tests (#3250)
     • feat(tools): improve `ask_questions` tool (#3248)
     • feat(adapters): add vision support to `kimi` (#3247)
     • fix(tools): auto-approve `run_command` tool (#3246)
     • tests: fix screenshots tests on `nightly` (#3245)
    
    nvim-lsp-config
     • fix(ast_grep): stop reuse_client from spawning duplicate lsp client #4480
     • docs: update generated annotations skip-checks: true
     • fix(eslint): convert workspaceFolder.uri to a proper URI #4479

--- twigs/jdtls/files/.config/nvim/lua/jdtls-load/init.lua
+++ twigs/jdtls/files/.config/nvim/lua/jdtls-load/init.lua
@@ -1,1 +1,13 @@
 vim.cmd('packadd! jdtls')
+vim.lsp.config('jdtls', {
+    capabilities = require'nvim-lspconfig-load'.capabilities(),
+    settings = {
+        java = {
+            inlayHints = {
+                formatParameters = {
+                    emabled = true,
+                },
+            },
+        },
+    },
+})
